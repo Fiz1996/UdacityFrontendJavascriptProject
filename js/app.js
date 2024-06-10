03cc6d37b6ede6e4b9d6e3763a9bcc2abb124608
Faisal Aleissa <mr.faisal.aleissa@gmail.com>
2024-06-10 05:46:57 +0300

    add a button to scroll to the top

--- js/app.js
+++ js/app.js
@@ -25,6 +25,17 @@
 const sections = document.querySelectorAll('section');
 const navList = document.getElementById('navbar__list');
 
+let span = document.querySelector(".up");
+window.onscroll = function() {
+    this.scrollY >= 1000 ? span.classList.add("show"): span.classList.remove("show") ;
+};
+span.onclick = function () {
+    window.scrollTo({
+        top:0,
+        behavior : "smooth",
+    });
+}
+
 /**
  * End Global Variables
  * Start Helper Functions
